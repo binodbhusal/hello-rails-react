9ea4f23976efbec674e066641c709e16b948020e
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-09-26 00:19:06 +0100

    Add App and Greeting components

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Ensure you're using Routes
+import Greeting from './Greeting';
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRandomGreeting } from '../redux/actions';
+
+function Greeting() {
+  const dispatch = useDispatch();
+  const greeting = useSelector(state => state.greeting.greeting); // Correct the state path
+
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Greeting:</h1>
+      <p>{greeting}</p>
+    </div>
+  );
+}
+
+export default Greeting;
