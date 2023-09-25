f93a7b5c9c3a49168fda154f5542d7c3aaa50c33
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-09-26 00:19:39 +0100

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './components/App';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
